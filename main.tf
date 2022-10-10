6c4c6e0cbc3280c463ccf8ee818bd702190ea5e2
jzjudith <jzjudith@github.com>
2022-10-10 22:47:26 +0100

    update

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "example" {
 
 # subnets
 resource "azurerm_subnet" "example" {
-  for_each             = var.subnet
+  for_each             = var.subnets
   name                 = each.key
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
